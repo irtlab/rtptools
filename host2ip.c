136175a36753f51b29df0bff623f6546dc679a5d
Henning Schulzrinne <hgs@cs.columbia.edu>
1999-02-14 15:36:02 +0000

    rtptools

---
+++ host2ip.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/socket.h>      /* struct sockaddr */
+#include <netdb.h>           /* gethostbyname() */
+#include <netinet/in.h>      /* sockaddr_in */
+#include <arpa/inet.h>       /* inet_addr() */
+#include <rpcsvc/ypclnt.h>   /* YP */
+#include "sysdep.h"
+
+static char rcsid[] = "$Id$";
+
+/*
+* Return IP address given host name 'host'.
+* If 'host' is "", set to INADDR_ANY.
+*/
+struct in_addr host2ip(char *host)
+{
+  struct in_addr in;
+  register struct hostent *hep;
+
+  /* Check whether this is a dotted decimal. */
+  if (!host || *host == '\0') {
+    in.s_addr = INADDR_ANY;
+  }
+  else if ((in.s_addr = inet_addr(host)) != -1) {
+  }
+  /* Attempt to resolve host name via DNS. */
+  else if ((hep = gethostbyname(host))) {
+    in = *(struct in_addr *)(hep->h_addr_list[0]);
+  }
+  /* As a last resort, try YP. */
+  else {
+    static char *domain = 0;  /* YP domain */
+    char *value;              /* key value */
+    int value_len;            /* length of returned value */
+
+    if (!domain) yp_get_default_domain(&domain);
+    if (yp_match(domain, "hosts.byname", host, strlen(host), &value, &value_len) == 0) {
+      in.s_addr = inet_addr(value);
+    }
+  }
+  return in;
+} /* host2ip */
