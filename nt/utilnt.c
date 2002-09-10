7e134cbf8fccac74775ee20d31310f040d67f2f9
Akira Tsukamoto <akira-t@s9.dion.ne.jp>
2002-09-10 10:08:20 +0000

    Add dummy-fd for WIN32 select(). Remove gettimeofday() from here to use hi-resolution version in gettimeofday.c.

--- nt/utilnt.c
+++ nt/utilnt.c
@@ -1,22 +1,48 @@
+#include <stdio.h>      /* fprintf(), stderr */
 #include <time.h>
-#include <sys/timeb.h>
-
+#include <sys/timeb.h>  /* ftime */
 #include "sysdep.h"
 
-int gettimeofday(struct timeval *tp, void *t) {
-  struct _timeb tv;
+/*
+ * For
+ *   1)startup winsock
+ *   2)winfd_dummy for select()
+ * by Akira 12/27/01
+ */
+int winfd_dummy = -1;       /* for select */
 
-  _ftime(&tv);
+void startupSocket(void)
+{
+  WSADATA wsaData;      /* WSAStartup() */
+
+  /* startup winsock */
+  if (WSAStartup(MAKEWORD(1, 1), &wsaData)) {
+    fprintf(stderr, "WSAStartup(): Could not start up WinSock.\n");
+    exit(1);
+  }
+
+  /* create dummy fd for select() */
+  winfd_dummy = socket(AF_INET, SOCK_DGRAM, 0);
+  if (winfd_dummy == INVALID_SOCKET) {
+    fprintf(stderr, "WSASocket: %d\n", WSAGetLastError ());
+    exit(1);
+  }
+
+}
+
+#if 0
+/* obsolete code */
+int gettimeofday(struct timeval *tp, void *dummy) {
+  struct timeb tv;
+
+  ftime(&tv);
   tp->tv_sec = tv.time;
   tp->tv_usec = tv.millitm*1000;
 
-  return 1;
-} 
+  return 0;
+}
+#endif
 
 ssize_t write_socket(int fildes, const void *buf, size_t nbyte) {
-	return send(fildes, buf, nbyte, 0);
+  return send(fildes, buf, nbyte, 0);
 }
-
-int sendmsg(int s, const struct msghdr *msg, int flags) {
-}
-
