c3e38bbf024a46635fe99e0eb84ee88fe41ee540
Akira Tsukamoto <akira-t@s9.dion.ne.jp>
2018-07-15 22:35:12 +0900

    For win to handle struct timeval

--- sysdep.h
+++ sysdep.h
@@ -102,6 +102,32 @@ extern int		gettimeofday(struct timeval*, void*);
 extern const char*	getprogname(void);
 extern void		setprogname(const char *);
 
+/* Win is missing these in #include <sys/time.h> */
+#ifndef timeradd
+#define timeradd(a, b, result)                                                \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;                          \
+    if ((result)->tv_usec >= 1000000)                                         \
+      {                                                                       \
+        ++(result)->tv_sec;                                                   \
+        (result)->tv_usec -= 1000000;                                         \
+      }                                                                       \
+  } while (0)
+#endif
+
+#ifndef timersub
+#define timersub(a, b, result)                                                \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
+
 #else /* not WIN32 */
 
 #include "config.h"
